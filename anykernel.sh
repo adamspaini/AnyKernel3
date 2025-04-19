d71d75ba31cd71afeb125e3b376862144d644633
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-19 03:53:52 -0400

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -5,6 +5,7 @@
 # begin properties
 properties() { '
 kernel.string=GoreKernel
+kernel.compiler=Crdroid Clang
 kernel.made=adams4d13
 kernel.version=4.14.3XX
 message.word=Flashing Done, You can now reboot.
@@ -22,16 +23,25 @@ supported.versions=11.0-15.0
 supported.patchlevels=
 '; } # end properties
 
+ui_print " "
+ui_print "- Kernel Name : GoreKernel"
+ui_print "- Kernel Version : 4.14.3XX"
+ui_print "- Compiler : Crdroid Clang"
+ui_print "- Device : vayu/bhima"
+ui_print "- Build Date : 2025-04-19"
+ui_print "- Maintainer : adams4d13"
+ui_print " "
+ui_print "- Preparing Environment Setup ..."
+ui_print " "
+
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
@@ -43,7 +53,6 @@ dump_boot;
 write_boot;
 ## end boot install
 
-
 # shell variables
 #block=vendor_boot;
 #is_slot_device=1;
@@ -52,10 +61,8 @@ write_boot;
 # reset for vendor_boot patching
 #reset_ak;
 
-
 ## AnyKernel vendor_boot install
 #split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
 
 #flash_boot;
 ## end vendor_boot install
-
